335ec78e4644e07106f580a029d1e4e5e5113131
Rado Hidayatulloh <acountgithub20@gmail.com>
2025-07-11 09:52:15 +0700

    perbaikan route

--- js/server/server.js
+++ js/server/server.js
@@ -30,6 +30,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, "127.0.0.1", () => {
-  console.log("Server berjalan di port 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server berjalan di port ${PORT}`);
 });
